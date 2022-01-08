06a5706cd07882328e3901ddd0f11ece0f6236ad
brien91022 <92264279+brien91022@users.noreply.github.com>
2022-01-08 21:16:20 +0800

    Create hw13_3.java

---
+++ hw13_3.java
@@ -0,0 +1,20 @@
+public class hw13_3
+{
+   public static void main(String args[])
+   {
+      try
+      {
+         int num=12,den=0;
+         int ans=num/den;
+         System.out.println("ans="+ans);
+      }
+      catch(ArithmeticException e)
+      {
+         System.out.println("°£¼Æ¬° 0");
+      }
+      finally
+      {
+         System.out.println("end of main() method!!");
+      }
+   }
+}
